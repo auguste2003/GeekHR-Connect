8d5b9cdcc056b9d5a85380326bde34c1862bf87e
aams01 <130369166+auguste2003@users.noreply.github.com>
2024-04-19 23:00:27 +0200

    Ajout du backend

---
+++ getthrconnet/src/main/java/fr/afrogeek/getthrconnet/GetthrconnetApplication.java
@@ -0,0 +1,13 @@
+package fr.afrogeek.getthrconnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetthrconnetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetthrconnetApplication.class, args);
+	}
+
+}
---
+++ getthrconnet/src/test/java/fr/afrogeek/getthrconnet/GetthrconnetApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.afrogeek.getthrconnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetthrconnetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
